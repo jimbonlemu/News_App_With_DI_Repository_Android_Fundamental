c089e75d662615fedd63cdefd96a680ae1c9f9ae
perompak_kode <iqbaltech29@gmail.com>
2024-03-28 02:01:32 +0700

    Initial commit

---
+++ app/src/main/java/com/jimbonlemu/newsapp/di/Injection.kt
@@ -0,0 +1,17 @@
+package com.jimbonlemu.newsapp.di
+
+import android.content.Context
+import com.jimbonlemu.newsapp.data.NewsRepository
+import com.jimbonlemu.newsapp.data.local.room.NewsDatabase
+import com.jimbonlemu.newsapp.data.remote.retrofit.ApiConfig
+import com.jimbonlemu.newsapp.utils.AppExecutors
+
+object Injection {
+    fun provideRepository(context: Context) {
+        val apiService = ApiConfig.getApiService()
+        val database = NewsDatabase.getInstance(context)
+        val dao = database.newsDao()
+        val appExecutors = AppExecutors()
+        return NewsRepository.getInstance(apiService, dao, appExecutors)
+    }
+}
